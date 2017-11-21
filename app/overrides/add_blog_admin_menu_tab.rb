9e18886d1babd00bf7fe0b8a5501cbe5fca4c526
Dinh Hieu <hieu.huynh@rubiik.com>
2017-11-21 10:34:29 +0700

    fix add blog to sidebar

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,8 +1,8 @@
 Deface::Override.new(:virtual_path => 'spree/layouts/admin',
                      :name => 'blog_admin_tabs',
                      :insert_bottom => '#main-sidebar',
-                     :text => 'spree/admin/shared/blogging_sidebar_menu',
-                     :disabled => false)
+                     :text => 'spree/admin/shared/blogging_sidebar_menu'
+                    )
 
 
 
