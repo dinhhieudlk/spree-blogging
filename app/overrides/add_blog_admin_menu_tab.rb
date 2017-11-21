e042129e357f63818c362a827b084e45c5e9fcb8
Dinh Hieu <hieu.huynh@rubiik.com>
2017-11-21 12:02:54 +0700

    fix sidebar admin

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "blog_admin_tabs",
                      :insert_bottom => "#main-sidebar",
-                     :text => "<% if Spree.user_class && can?(:admin, Spree::BlogEntry) %><ul class='nav nav-sidebar'><%= tab Spree.t(:BLOG), url: spree.admin_blog_entries_path, icon: 'comment', label: plural_resource_name(Spree::Blog) %></ul><% end %>",
+                     :text => "<% if Spree.user_class && can?(:admin, Spree::BlogEntry) %><ul class='nav nav-sidebar'><%= tab Spree.t(:BLOG), url: spree.admin_blog_entries_path, icon: 'comment', label: plural_resource_name(Spree::BlogEntry) %></ul><% end %>",
 										)
 
 
