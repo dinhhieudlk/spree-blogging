818632b82238f97cabc2093c3f042a26de2b4c0b
Dinh Hieu <hieu.huynh@rubiik.com>
2017-11-21 10:26:49 +0700

    change directory sidebar

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,7 +1,7 @@
-Deface::Override.new(:virtual_path => "spree/layouts/admin",
-                     :name => "blog_admin_tabs",
-                     :insert_bottom => "#main-sidebar",
-                     :text => "spree/admin/shared/blogging_sidebar_menu",
+Deface::Override.new(:virtual_path => 'spree/layouts/admin',
+                     :name => 'blog_admin_tabs',
+                     :insert_bottom => '#main-sidebar',
+                     :text => 'spree/admin/shared/blogging_sidebar_menu',
                      :disabled => false)
 
 
